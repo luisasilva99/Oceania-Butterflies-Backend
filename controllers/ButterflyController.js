ddc1045c14111587aea2b39fbcd2f2426a1544d6
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-08-29 12:34:48 +0200

    added methods GET, GET ONE and DELETE (#5)

---
+++ controllers/ButterflyController.js
@@ -0,0 +1,52 @@
+import ButterflyModel from "../models/ButterflyModel.js"
+
+// GET all butterflies
+export const getAllButterflies = async(req, res) => {
+    try {
+        const butterflies = await ButterflyModel.findAll()
+        res.status(200).json(butterflies)
+    } catch  (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+
+//GET one butterfly
+const getOneButterfly = async(req, res) => {
+    try {
+        const { id } = req.params;  // sacamos el id de la URL
+        const butterfly = await ButterflyModel.findByPk(id)  // buscamos por la PK
+        
+        if (!butterfly) { //si NO existe
+            return res.status(404).json({ message: "Butterfly not found"});
+        }
+        res.status(200).json(butterfly); // si existe la enviamos
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+//DELETE one butterfly
+const deleteButterfly = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const butterfly = await 
+        ButterflyModel.findByPk(id);
+
+        if (!butterfly) {
+            return res.status(404).json({ message: "Butterfly not found"});
+        }
+        
+        await butterfly.destroy();
+        return res.status(200).json({ message: "Butterfly deleted successfully"})
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error});
+    }
+}
+//POST one butterfly
+const createButterfly = async() => {
+    
+}
+//UPDATE one butterfly
+const updateButterfly = async() => {
+    
+}
