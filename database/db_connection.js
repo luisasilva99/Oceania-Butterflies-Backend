e684b61d692652c49fee43a82fcb1dcad3e5e786
irinatiron <irinatirongiurgiu@gmail.com>
2025-09-02 15:49:16 +0200

    fix(db_connection.js): tables error (#4)

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,31 +1,21 @@
 import { Sequelize } from "sequelize";
-import '@dotenvx/dotenvx/config';
+import '@dotenvx/dotenvx/config'; 
 
 const db_connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME,     
+  process.env.DB_USER,     
+  process.env.DB_PASSWORD, 
   {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,     
+    dialect: process.env.DB_DIALECT, 
     define: {
-      timestamps: false,
-    },
-    logging: console.log, 
+      timestamps: false 
+    }
   }
 );
 
-const initDB = async () => {
-  try {
-    await db_connection.authenticate();
-    console.log("✅ Connection established successfully.");
-    await db_connection.sync({ alter: true }); // crea las tablas si no existen o las actualiza
-    console.log("✅ Tables synchronized successfully.");
-  } catch (err) {
-    console.error("❌ Error connecting to the database:", err);
-  }
-};
-
-initDB();
+db_connection.authenticate()
+  .then(() => console.log("✅ Connection established successfully."))
+  .catch(err => console.error("❌ Error connecting to the database:", err));
 
 export default db_connection;
