07c600b2e955bb2886c4af1d72ae3563d67a60d5
irinatiron <irinatirongiurgiu@gmail.com>
2025-09-02 15:14:55 +0200

    fix(merge)

--- routes/butterflyRoutes.js
+++ routes/butterflyRoutes.js
@@ -1,11 +1,7 @@
 import express from 'express'
 import { 
-<<<<<<< HEAD
     getAllButterflies, 
     getOneButterfly,
-=======
-    getAllButterfly, 
->>>>>>> d3dae850be73d715ee215db1ab7a7131a2b55548
     createButterfly, 
     deleteButterfly, 
     updateButterfly
@@ -14,14 +10,10 @@ import {
 const butterflyRouter = express.Router()
 
 // GET - Obtener todos las mariposas
-<<<<<<< HEAD
 butterflyRouter.get('/', getAllButterflies)
 
 // GET - Obtener una mariposa por ID
 butterflyRouter.get('/:id', getOneButterfly)
-=======
-butterflyRouter.get('/', getAllButterfly)
->>>>>>> d3dae850be73d715ee215db1ab7a7131a2b55548
 
 // POST - Crear una nueva mariposa
 butterflyRouter.post('/', createButterfly)
