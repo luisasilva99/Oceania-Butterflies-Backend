4945d4dad588095abf3f3b4b4f6cdb65a3f765bd
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-09-09 10:07:46 +0200

    test GET ONE

---
+++ test/butterflies.test.js
@@ -0,0 +1,58 @@
+import request from "supertest";
+import { app, server } from "../app.js"
+import db_connection from "../database/db_connection.js";
+import ButterflyModel from "../models/ButterflyModel.js";
+
+describe("butterfly crud", () => {
+    let testButterfly; //guardamos la mariposa creada en la base de datos
+
+    //conectamos la bd y creamos un registro de prueba
+    beforeAll(async () => {
+        await db_connection.authenticate()
+
+        testButterfly = await ButterflyModel.create({
+            commonName: "TestButterfly",
+            scientificName: "TestButterfly",
+            family: "TestButterfly",
+            region: "TestButterfly",
+            specificLocation: "TestButterly",
+            threatLevel: "TestButterfly"
+        })
+    })
+    afterAll(async () => {
+        if (testButterfly) {
+            await testButterfly.destroy(); //limpiamos la mariposa de prueba
+        }
+        await db_connection.close();
+        server.close();
+    });
+    //test GET ONE
+    describe("GET /butterflies/:id", () => {
+        let response
+
+        beforeEach(async () => {
+            response = await request(app).get(`/butterflies/${testButterfly.id}`)
+        })
+        it('should return status 200', async () => {
+            expect(response.status).toBe(200)
+        })
+
+        it("should return butterfly with correct id", async () => {
+            expect(response.body.id).toBe(testButterfly.id);
+        });
+
+        it('should return butterfly with required fields', async () => {
+            expect(response.body).toMatchObject({
+                id: testButterfly.id,
+                commonName: "TestButterfly",
+                scientificName: expect.any(String),
+                family: expect.any(String),
+                region: expect.any(String),
+                specificLocation: expect.any(String),
+                threatLevel: expect.any(String)
+
+            });
+        });
+    });
+
+});
